cc81cb964f9d25bfaa9cbb51c86d5079c1a5a326
NurgazievichR <zalalidinovroma@gmail.com>
2025-05-25 20:16:31 +0600

    model user

---
+++ src/users/models.py
@@ -0,0 +1,11 @@
+from src.database.base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    username: Mapped[str] = mapped_column(String(64), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(256), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(256), nullable=False)
