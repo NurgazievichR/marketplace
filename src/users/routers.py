9e93319a21aa90c256a75944a8820b6578a40ce0
NurgazievichR <zalalidinovroma@gmail.com>
2025-05-26 18:27:21 +0600

    redis handler

--- src/users/routers.py
+++ src/users/routers.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 
@@ -13,3 +13,18 @@ async def list_users(db_session: AsyncSession = Depends(get_db)):
     result = await db_session.execute(select(User))
     users = result.scalars().all()
     return users
+
+@router.get("/redis")
+async def get_all_redis_data(request: Request):
+    redis = request.app.redis 
+    keys = await redis.keys("*")
+
+    if not keys:
+        return {}
+
+    result = {}
+    for key in keys:
+        value = await redis.get(key)
+        result[key] = value
+
+    return result
